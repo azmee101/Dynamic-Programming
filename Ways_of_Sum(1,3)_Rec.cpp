0775a12b67152491434b5bef2e97bdea3eb4fd72
azmee101 <iazmee101@gmail.com>
2025-11-23 00:13:04 +0600

    Number of Ways of Constructing Sum using 1 and 3

---
+++ Ways_of_Sum(1,3)_Rec.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+const ll MOD = 1e9+7;
+
+#define NEED_FOR_SPEED ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+int ways(int i){
+    if(i == 1 || i == 2) return 1;
+    if(i == 3) return 2;
+    return ways(i-1) + ways(i-3);
+}
+
+void solve(){
+    int n; cin >> n;
+    cout << ways(n) << '\n';
+    return;
+}
+
+int32_t main()
+{
+    NEED_FOR_SPEED;
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
+
+// Count of different ways to express N as the sum of 1, 3
